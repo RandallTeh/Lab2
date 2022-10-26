bee2031b825a45ca78c8a77ecc500f6f1a69d191
Randall Teh <tehrandall@gmail.com>
2022-10-26 16:09:40 +0800

    2nd commit

--- bmi.py
+++ bmi.py
@@ -1,4 +1,17 @@
 def calculate_bmi(height, weight):
- print("Height = " + height)
- print("Weight = " + weight)
-calculate_bmi(weight="57", height="1.73")
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+
+ BMI = weight / (height * height)
+
+ if BMI < 18.5:
+  print("underweight")
+ elif BMI >= 18.5 and BMI <= 25:
+  print("normalweight")
+ else:
+  print("overweight")
+
+
+ print(BMI)
+
+calculate_bmi(weight=57, height=1.73)
