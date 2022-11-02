f8bd1b3696f08e2960b9a2723936ab38915d3762
Randall Teh <tehrandall@gmail.com>
2022-11-02 13:19:07 +0800

    Lab2_v1.2 release

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,6 @@
+import statistics
+
+
 def display_main_menu():
     x = input("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
     return x
@@ -21,15 +24,16 @@ def find_min_max(floats):
 
     print("Lowest temp is: " + str(min_value))
     print("Highest temp is: " + str(max_value))
-    print("find_min_max")
 
-'''
-def sort_temperature():
-    print("sort_temperature")
+def sort_temperature(floats):
+    asc = sorted(floats)
+    return asc
 
-def calc_median_temperature():
-    print("calc_median_temperature")
-'''
+
+def calc_median_temperature(asc):
+    mn = statistics.median(asc)
+    print("The Median is " + str(mn))
+
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -37,8 +41,8 @@ def main():
     num_list = get_user_input(x)
     calc_average(num_list)
     find_min_max(num_list)
-#    sort_temperature()
-#    calc_median_temperature()
+    asc = sort_temperature(num_list)
+    calc_median_temperature(asc)
 
 
 if __name__ == "__main__":
